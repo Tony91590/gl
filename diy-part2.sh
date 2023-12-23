e8dcc1dd371b9f7ca79394ff016da3a2e7fd8974
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2023-12-23 22:13:04 +0100

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,6 +23,7 @@ cp $GITHUB_WORKSPACE/lean/GL-MT6000/data/ddns.config feeds/packages/net/ddns-scr
 cp $GITHUB_WORKSPACE/lean/GL-MT6000/data/default-settings/zzz-default-settings package/lean/default-settings/files/
 cp $GITHUB_WORKSPACE/lean/GL-MT6000/data/default-settings/Makefile package/lean/default-settings/
 #cp $GITHUB_WORKSPACE/lean/GL-MT6000/data/autocore/index.htm package/lean/autocore/files/arm/
+chmod 0644 target/linux/mediatek/filogic/base-files/lib/preinit/81_fix_eeprom
 cp $GITHUB_WORKSPACE/lean/GL-MT6000/data/zones.lua feeds/luci/applications/luci-app-firewall/luasrc/model/cbi/firewall/
 cat > package/base-files/files/etc/banner << EOF
   _______                     ________        __
